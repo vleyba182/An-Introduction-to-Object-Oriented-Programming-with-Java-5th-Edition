d15b7abef877cc775360ed125cd29af878e54578
vleyba182@gmail.com <zaeod]aAfDJDae&YWAuyzvDhoruViLJAMbAYL7yUpRZhQvAPR8>
2016-03-01 20:09:02 -0700

    Added exercises.

---
+++ src/Chapter_2/Ch2Level1Exercise1.java
@@ -0,0 +1,27 @@
+/**
+ * Created by vicenteleyba on 3/1/16.
+ *
+ * Chapter 2 Level 1 Exercise 1:
+ * Write a program that displays a frame window 800
+ * pixels wide and 600pixels high. Set the title of
+ * the frame to Welcome to Java.
+ *
+ * */
+
+
+import javax.swing.*;
+
+class Ch2SLevel1Exercise1 {
+
+    public static void main(String[] args) {
+
+        JFrame myWindow;
+
+        myWindow = new JFrame();
+
+        myWindow.setSize(800, 600);
+        myWindow.setTitle("Welcome to Java!");
+        myWindow.setVisible(true);
+    }
+
+}
